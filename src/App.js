24309023a2611d4b9493a65599c73416dd959c17
Pablo Viojo <pablo@tiopaul.io>
2021-05-19 15:59:53 -0400

    Lamp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Lamp from './components/Lamp'
-import styles from './index.module.css'
-export default function App() {
+import Lamp from "./components/Lamp";
+import styles from "./index.module.css";
+const App = () => {
   return (
     <div className={styles.cnt}>
-      <Lamp
-        color="#fff"
-        intensity={10}
-      >
+      <Lamp color="#fff" intensity={50} />
+      <ColorSelector />
     </div>
   );
-}
+};
+
+export default App;
--- src/components/Lamp/index.js
+++ src/components/Lamp/index.js
@@ -1,5 +1,17 @@
 import styles from "./index.module.css";
 
 const Lamp = ({ color, intensity }) => {
-  return <div className={styles.lamp}></div>;
+  return (
+    <div className={styles.lamp}>
+      <div
+        className={styles.inner}
+        style={{
+          backgroundColor: color,
+          opacity: intensity / 100
+        }}
+      ></div>
+    </div>
+  );
 };
+
+export default Lamp;
